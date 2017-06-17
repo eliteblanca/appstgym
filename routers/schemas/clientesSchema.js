40758ebf34f74df826de0db354a1584e2b149d8e
eliteblanca <juliannvargas1@gmail.com>
2017-06-17 17:54:14 -0500

    areglo unique fliends cliente

--- routers/schemas/clientesSchema.js
+++ routers/schemas/clientesSchema.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var subSchema = require('./subscripcionSchema.js');
 var clientesSchema = mongoose.Schema({
 	nombre:{type:'String',required:true},
-	identificacion:{type:'String',required:true,unique:true},
+	identificacion:{type:'String',required:true,unique:true,dropDups: true},
 	gimnacio:{ type: mongoose.Schema.Types.ObjectId, ref: 'usuario'},
 	subscripcion:[subSchema],
 	sexo:{type:'String',required:false},
